75547e6387c0443b0b149aef63f44d9ff974c29c
popov <aptem336@gmail.com>
2022-04-29 22:16:45 +0800

    routes impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import {Create} from './components/Create'
+import {Create as Read} from './components/Create'
+import {Create as Update} from './components/Create'
+// @ts-ignore
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <Create/>
-    </div>
-  );
+    return (
+        <Router>
+            <Route exact path='/create' component={Create}/>
+            <Route exact path='/read' component={Read}/>
+            <Route exact path='/update/:id' component={Update}/>
+        </Router>
+    );
 }
 
 export default App;
